35b348477908989d2e145ddbcc9cc16f9f6e6278
grk_rise <grk.rise@gmail.com>
2022-12-07 17:10:21 +0300

    Add Calculator.java by Kurtseva

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,17 +1,43 @@
+import java.util.Scanner;
+
 public class Calculator {
-    public int add(int a, int b){
-        //TODO inser your realisation in method add
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2;
+        String operator;
+
+        System.out.println("Введите первое число: ");
+        num1 = scanner.nextDouble();
+
+        System.out.println("Введите оператор (+, -, *, /): ");
+        operator = scanner.next();
+
+        System.out.println("Введите второе число: ");
+        num2 = scanner.nextDouble();
+
+        double result = calculate(num1, num2, operator);
+        System.out.println("Результат: " + result);
     }
-    public int dif(int a, int b){
-        //TODO inser your realisation in method dif
-    }
-    public int div(int a, int b){
-        //TODO inser your realisation in method div
-    }
-    public int times(int a, int b){
-        //TODO inser your realisation in method times
-    }
-    public int solver(){
-        //TODO inser your realisation in method solver
+
+    public static double calculate(double num1, double num2, String operator) {
+        switch (operator) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                if (num2 != 0) {
+                    return num1 / num2;
+                } else {
+                    System.out.println("Ошибка: Деление на ноль!");
+                    return 0;
+                }
+            default:
+                System.out.println("Ошибка: Неверный оператор!");
+                return 0;
+        }
     }
 }
